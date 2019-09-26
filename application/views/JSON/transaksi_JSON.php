4a72e21f0cd020b4af81b4f6d457ab780cef8011
mrayhanhakim <mrayhanhakim2@gmail.com>
2019-09-27 05:07:21 +0700

    Disable scroll on input number, change total price on keyup/click, add submit button

--- application/views/JSON/transaksi_JSON.php
+++ application/views/JSON/transaksi_JSON.php
@@ -1,4 +1,6 @@
 <script>
+	/* Global */
+	let total = 0;
 	$(document).ready(function(){
 		load_data();
 
@@ -14,7 +16,7 @@
 		}
 
 		$('#search_item').keyup(function() {
-			var search = $(this).val();
+			let search = $(this).val();
 
 			if (search != '') {
 				load_data(search);
@@ -22,41 +24,62 @@
 				load_data();
 			}
 		})
-
-		
 	});
 
 	function add_item(id_obat) {
+		// Enable submit button when first item is added
+		if ($("#item_buy_list").children().length === 0) {
+			$("#transaksi_submit").removeAttr("disabled");
+		}
+
 		$.ajax({
 			url: "<?php echo base_url() ?>transaksi/check_data",
 			method: "POST",
 
 			data: {id_obat:id_obat},
 			success:function(result) {
-				console.log(result);
-				var data = JSON.parse(result);
+				let data = JSON.parse(result);
 				$('#item_buy_list').append(
 					'<tr>' + 
 						'<td class="col-md-6">' + data.nama_obat + '</td>'+
-						'<td class="col-md-2">'+
-							// '<input id="'+data.id_obat+'Val" type="number" onkeyup="sum_value('+ $("#" + data.id_obat + "Val").val() +')" value="1" max="'+data.sisa_stok+'">'+
-							// '<input id="'+data.id_obat+'Val" type="number" onkeypress="sum_value()" value="1" max="'+data.sisa_stok+'">'+
-							'<input id="'+data.id_obat+'Val" type="number" onchange="sum_value(\'#'+data.id_obat+'Val\','+data.harga_jual+' )" min="0" value="0" max="'+data.sisa_stok+'">'+
+						'<td class="col-md-2 input_n_obat">'+
+							'<input id="'+data.id_obat+'_n" type="number" min="1" value="1" max="'+data.sisa_stok+'">'+
 						'</td>'+
-						'<td class="col-md-4" id="price'+data.id_obat+'">' + data.harga_jual + '</td>'+
+						'<td class="col-md-4"> Rp. ' + numberWithCommas(data.harga_jual) + 
+							'<input type="hidden" id="harga_'+data.id_obat+'_n" value="'+data.harga_jual+'">'+
+						'</td>'+
 					'</tr>'
 				);
 				$('#'+data.id_obat+'').attr("disabled", true);
-				sum_value('#'+data.id_obat+'',data.harga_jual);
+				$('#'+data.id_obat+'').attr("onclick", false);
+				const harga = parseInt(data.harga_jual);
+				total += harga ;
+				$("#totalHarga").text(numberWithCommas(total));  
+
+				$('input[type=number]').on('mousewheel',function(e){ $(this).blur(); });
 			}
 		})
 	}
 
-	function sum_value(id, harga_jual) {
-		var count = $(id).val();
-		var sum = count * harga_jual;
-		$('#totalHarga').text('Rp. '+sum);
-		
+	// Sum the total of price in item list
+	$(document).on('change keyup', 'input[type=number]', function() {
+		const id = $(this).attr('id');
+		const harga = $('#harga_'+id+'').val();
+		const prev_n = this.defaultValue;
+		const n = $('#'+id+'').val();
+		if (prev_n < n) {
+			total += harga * (n - prev_n);
+		} else if (prev_n > n) {
+			total -= harga * (prev_n - n);
+		}
+		$('#'+id+'').attr('value', n);
+		$("#totalHarga").text(numberWithCommas(total));
+	})
 
+	// Add commas to number
+	function numberWithCommas(x) {
+    var parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
 	}
 </script>
--- application/views/transaksi_view.php
+++ application/views/transaksi_view.php
@@ -8,7 +8,9 @@
     <div class="panel col-md-5">
         <div class="input-group" style="width: 100%; margin: 10px 0px;">
             <input type="text" name="search_item" id="search_item" value="" class="form-control" placeholder="Cari Item...">
-            <span class="input-group-btn"><button type="button" style="width: 100%" class="btn btn-primary">Go</button></span>
+            <span class="input-group-btn">
+                <button type="button" style="width: 100%" class="btn btn-primary">Go</button>
+            </span>
         </div>
         <div id="result"></div>
     </div>
@@ -27,7 +29,12 @@
                 <tbody>
                     <tr>
                         <th colspan="2">Total Harga</th>
-                        <th id="totalHarga">Rp. </th>
+                        <th>Rp. <span id="totalHarga">0</span></th>
+                    </tr>
+                    <tr>
+                        <th colspan="3" class="text-right">
+                            <a class="btn btn-md btn-info" id="transaksi_submit" disabled>Submit</a>
+                        </th>
                     </tr>
                 </tbody>
             </table>
