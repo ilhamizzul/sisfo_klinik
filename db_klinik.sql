5bae39e507f07417d7098dc87b98d50188aa1962
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2019-12-30 15:22:02 +0700

    Update db_klinik.sql

--- db_klinik.sql
+++ db_klinik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 20, 2019 at 09:37 AM
+-- Generation Time: Dec 30, 2019 at 09:21 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.0
 
@@ -47,17 +47,9 @@ CREATE TABLE `tb_obat` (
   `id_obat` varchar(11) NOT NULL,
   `nama_obat` varchar(20) NOT NULL,
   `harga_jual` int(11) NOT NULL,
-  `harga_beli` int(11) NOT NULL
+  `harga_beli` int(11) NOT NULL,
+  `delete_status` enum('true','false') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tb_obat`
---
-
-INSERT INTO `tb_obat` (`id_obat`, `nama_obat`, `harga_jual`, `harga_beli`) VALUES
-('ait', 'Aito obat tetes mata', 15000, 9800),
-('all1', 'Allopurinol 100mg', 500, 220),
-('er3', 'eropams', 12000, 10000);
 
 -- --------------------------------------------------------
 
@@ -75,16 +67,6 @@ CREATE TABLE `tb_pasien` (
   `nama_kepala_keluarga` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `tb_pasien`
---
-
-INSERT INTO `tb_pasien` (`id_pasien`, `nama_pasien`, `alamat`, `nomor_telepon`, `tempat_lahir`, `tanggal_lahir`, `nama_kepala_keluarga`) VALUES
-(3, 'Reza Faisal M.', 'Malang, Jawa Timur, Indonesia', '085335831672', 'Kraksaan', '2019-06-02', 'hermanto'),
-(4, 'Ramadhani Nurul Amalia', 'balalalalala', '', '$this->dashboard_model->count_pemeriksaan_bulan()', '2019-07-08', '$this->dashboard_model->count_pemeriksaa'),
-(5, 'Alief Aulia Rachman', '$this->dashboard_model->count_pemeriksaan_bulan()', '', '$this->dashboard_model->count_pemeriksaan_bulan()', '2019-07-01', '$this->dashboard_model->count_pemeriksaa'),
-(6, 'budi', 'dcbsjdna', '085335831726', 'malang', '2019-08-14', 'hermanto');
-
 -- --------------------------------------------------------
 
 --
@@ -101,28 +83,6 @@ CREATE TABLE `tb_pemeriksaan` (
   `id_pasien` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `tb_pemeriksaan`
---
-
-INSERT INTO `tb_pemeriksaan` (`id_pemeriksaan`, `tanggal_pemeriksaan`, `hasil_pemeriksaan`, `diagnosis`, `terapi`, `status_transaksi`, `id_pasien`) VALUES
-(6, '2019-07-05', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', 'belum', 3),
-(13, '2019-06-12', '\"DATE_FORMAT(column_name,\'%Y-%m\')\"', '\"DATE_FORMAT(column_name,\'%Y-%m\')\"', '\"DATE_FORMAT(column_name,\'%Y-%m\')\"', 'belum', 3),
-(16, '2018-07-07', 'blalalalalalalablalalalalalala', 'blalalalalalalablalalalalalala', 'blalalalalalalablalalalalalala', 'sudah', 3),
-(17, '2019-06-13', '$this->uri->total_segments() < 3', '$this->uri->total_segments() < 3', '$this->uri->total_segments() < 3', 'sudah', 3),
-(18, '2019-07-12', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', 'sudah', 3),
-(20, '2019-07-12', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', 'sudah', 4),
-(21, '2019-07-12', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', 'sudah', 5),
-(23, '2019-07-11', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', 'sudah', 4),
-(24, '2019-07-03', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', 'sudah', 5),
-(25, '2019-07-01', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', 'sudah', 4),
-(28, '2019-07-07', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', 'sudah', 5),
-(29, '2019-07-07', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', 'sudah', 5),
-(30, '2019-07-14', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', 'sudah', 5),
-(31, '2019-07-14', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', 'sudah', 4),
-(33, '2019-07-16', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', '$this->dashboard_model->count_pemeriksaan_bulan()', 'sudah', 3),
-(34, '2019-08-04', 'lorem ipsum', 'lorem ipsum', 'lorem ipsum', 'sudah', 6);
-
 -- --------------------------------------------------------
 
 --
@@ -137,17 +97,6 @@ CREATE TABLE `tb_stok_obat` (
   `sisa_stok` int(11) NOT NULL,
   `bulan` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tb_stok_obat`
---
-
-INSERT INTO `tb_stok_obat` (`id_stok`, `id_obat`, `stok_masuk`, `stok_keluar`, `sisa_stok`, `bulan`) VALUES
-(10, 'ait', 12, 0, 12, '2019-08-27'),
-(12, 'ait', 60, 30, 35, '2019-09-17'),
-(13, 'ait', 45, 27, 65, '2019-06-10'),
-(14, 'all1', 30, 0, 30, '2019-09-06'),
-(15, 'er3', 12, 0, 12, '2019-09-13');
 
 -- --------------------------------------------------------
 
@@ -216,7 +165,7 @@ ALTER TABLE `tb_transaksi`
 -- AUTO_INCREMENT for table `tb_detail_transaksi`
 --
 ALTER TABLE `tb_detail_transaksi`
-  MODIFY `id_detail_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_detail_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `tb_pasien`
@@ -228,19 +177,19 @@ ALTER TABLE `tb_pasien`
 -- AUTO_INCREMENT for table `tb_pemeriksaan`
 --
 ALTER TABLE `tb_pemeriksaan`
-  MODIFY `id_pemeriksaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id_pemeriksaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT for table `tb_stok_obat`
 --
 ALTER TABLE `tb_stok_obat`
-  MODIFY `id_stok` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_stok` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 
 --
 -- AUTO_INCREMENT for table `tb_transaksi`
 --
 ALTER TABLE `tb_transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- Constraints for dumped tables
